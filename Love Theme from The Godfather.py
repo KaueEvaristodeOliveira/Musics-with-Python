35b2ea0e21e5bc8e71bd74ca0da3b4285c074f03
Kaue Evaristo <69019326+KaueEvaristodeOliveira@users.noreply.github.com>
2022-07-05 16:09:48 -0300

    Update Love Theme from The Godfather.py

--- Love Theme from The Godfather.py
+++ Love Theme from The Godfather.py
@@ -14,182 +14,175 @@ winsound.Beep(1000, 60)
 time.sleep(0.5)
 winsound.Beep(1000, 60)
 time.sleep(0.5)
-winsound.Beep(100, 240)
-time.sleep(0.5)
-winsound.Beep(1000, 60)
-time.sleep(0.5)
-winsound.Beep(1000, 60)
-time.sleep(0.5)
 
 #First Part
-winsound.Beep(220, 780)#LÁ-cima
+winsound.Beep(220, 780)#A-cima
 time.sleep(0.1)
-winsound.Beep(293, 520)#RÉ-cima
-winsound.Beep(349, 520)#FÁ-cima
+winsound.Beep(293, 520)#D-cima
+winsound.Beep(349, 520)#F-cima
 time.sleep(0.1)
-winsound.Beep(329, 520)#MI-cima
-winsound.Beep(293, 520)#RÉ-cima
-winsound.Beep(349, 520)#FÁ-cima
-winsound.Beep(293, 520)#RÉ-cima
+winsound.Beep(329, 520)#E-cima
+winsound.Beep(293, 520)#D-cima
+winsound.Beep(349, 520)#F-cima
+winsound.Beep(293, 520)#D-cima
 time.sleep(0.1)
-winsound.Beep(329, 520)#MI-cima
-winsound.Beep(293, 520)#RÉ-cima
-winsound.Beep(233, 520)#SiB-cima
-winsound.Beep(261, 520)#DÓ-cima
+winsound.Beep(329, 520)#E-cima
+winsound.Beep(293, 520)#D-cima
+winsound.Beep(233, 520)#BB-cima
+winsound.Beep(261, 520)#C-cima
 time.sleep(0.1)
-winsound.Beep(220, 840)#LÁ-cima
+winsound.Beep(220, 840)#A-cima
 time.sleep(0.4)
 
 #Second Part
-winsound.Beep(220, 780)#LÁ-cima
-winsound.Beep(293, 520)#RÉ-cima
-winsound.Beep(349, 520)#FÁ-cima
+winsound.Beep(220, 780)#A-cima
+winsound.Beep(293, 520)#D-cima
+winsound.Beep(349, 520)#F-cima
 time.sleep(0.1)
-winsound.Beep(329, 520)#MI-cima
-winsound.Beep(293, 520)#RÉ-cima
-winsound.Beep(349, 520)#FÁ-cima
-winsound.Beep(293, 520)#RÉ-cima
+winsound.Beep(329, 520)#E-cima
+winsound.Beep(293, 520)#D-cima
+winsound.Beep(349, 520)#F-cima
+winsound.Beep(293, 520)#D-cima
 time.sleep(0.1)
-winsound.Beep(329, 520)#MI-cima
-winsound.Beep(293, 520)#RÉ-cima
-winsound.Beep(220, 520)#LÁ-cima
-winsound.Beep(207, 580)#LáB-cima
+winsound.Beep(329, 520)#E-cima
+winsound.Beep(293, 520)#D-cima
+winsound.Beep(220, 520)#A-cima
+winsound.Beep(207, 580)#AB-cima
 time.sleep(0.1)
-winsound.Beep(196, 840)#SOL-cima
+winsound.Beep(196, 840)#G-cima
 time.sleep(0.4)
 
 #Third Part
-winsound.Beep(196, 780)#SOL-cima
-winsound.Beep(233, 520)#SiB-cima
-winsound.Beep(261, 520)#Do#-cima
+winsound.Beep(196, 780)#G-cima
+winsound.Beep(233, 520)#BB-cima
+winsound.Beep(261, 520)#C#-cima
 time.sleep(0.1)
-winsound.Beep(329, 840)#MI-cima
+winsound.Beep(329, 840)#E-cima
 time.sleep(0.4)
 
 #Fourth Part
-winsound.Beep(196, 780)#SOL-cima
-winsound.Beep(233, 520)#SiB-cima
-winsound.Beep(261, 520)#Do#-cima
+winsound.Beep(196, 780)#G-cima
+winsound.Beep(233, 520)#BB-cima
+winsound.Beep(261, 520)#C#-cima
 time.sleep(0.1)
-winsound.Beep(293, 840)#RÉ-cima
+winsound.Beep(293, 840)#D-cima
 time.sleep(0.3)
 
 #Fifth Part
-winsound.Beep(146, 780)#RÉ-baixo
+winsound.Beep(146, 780)#D-baixo
 time.sleep(0.1)
-winsound.Beep(174, 520)#FÁ-baixo
-winsound.Beep(261, 520)#DÓ-cima
+winsound.Beep(174, 520)#F-baixo
+winsound.Beep(261, 520)#C-cima
 #CODA----------------------------
 time.sleep(0.1)
-winsound.Beep(233, 520)#SiB-cima
-winsound.Beep(220, 520)#LÁ-cima
-winsound.Beep(261, 520)#DÓ-cima
-winsound.Beep(233, 520)#SiB-cima
+winsound.Beep(233, 520)#BB-cima
+winsound.Beep(220, 520)#A-cima
+winsound.Beep(261, 520)#C-cima
+winsound.Beep(233, 520)#BB-cima
 time.sleep(0.1)
-winsound.Beep(233, 520)#SiB-cima
-winsound.Beep(220, 520)#LÁ-cima
-winsound.Beep(220, 520)#LÁ-cima
-winsound.Beep(130, 520)#Dó#-baixo
+winsound.Beep(233, 520)#BB-cima
+winsound.Beep(220, 520)#A-cima
+winsound.Beep(220, 520)#A-cima
+winsound.Beep(130, 520)#C#-baixo
 time.sleep(0.1)
-winsound.Beep(146, 840)#RÉ-baixo
+winsound.Beep(146, 840)#D-baixo
 time.sleep(0.3)
 
 #Sixth Part
-winsound.Beep(293, 840)#RÉ-cima
-winsound.Beep(293, 720)#RÉ-cima
-winsound.Beep(277, 720)#RéB-cima
+winsound.Beep(293, 840)#D-cima
+winsound.Beep(293, 720)#D-cima
+winsound.Beep(277, 720)#DB-cima
 time.sleep(0.1)
-winsound.Beep(261, 720)#DÓ-cima
+winsound.Beep(261, 720)#C-cima
 time.sleep(0.1)
-winsound.Beep(329, 720)#MI-cima
+winsound.Beep(329, 720)#E-cima
 time.sleep(0.1)
-winsound.Beep(293, 720)#RÉ-cima
-winsound.Beep(233, 720)#SiB-cima
+winsound.Beep(293, 720)#D-cima
+winsound.Beep(233, 720)#BB-cima
 time.sleep(0.1)
-winsound.Beep(220, 840)#LÁ-cima
+winsound.Beep(220, 840)#A-cima
 time.sleep(0.3)
 
 #Seventh Part
-winsound.Beep(220, 780)#LÁ-cima
-winsound.Beep(261, 520)#DÓ-cima
-winsound.Beep(220, 520)#LÁ-cima
+winsound.Beep(220, 780)#A-cima
+winsound.Beep(261, 520)#C-cima
+winsound.Beep(220, 520)#A-cima
 time.sleep(0.1)
-winsound.Beep(196, 840)#SOL-cima
+winsound.Beep(196, 840)#G-cima
 time.sleep(0.3)
 
 #Octave Part
-winsound.Beep(196, 780)#SOL-cima
-winsound.Beep(233, 520)#SiB-cima
-winsound.Beep(207, 520)#Sol#-cima
+winsound.Beep(196, 780)#G-cima
+winsound.Beep(233, 520)#BB-cima
+winsound.Beep(207, 520)#G#-cima
 time.sleep(0.1)
-winsound.Beep(220, 840)#LÁ-cima
+winsound.Beep(220, 840)#A-cima
 time.sleep(0.3)
 
 #Ninth Part
-winsound.Beep(220, 780)#LÁ-cima
+winsound.Beep(220, 780)#A-cima
 time.sleep(0.1)
-winsound.Beep(293, 520)#RÉ-cima
-winsound.Beep(349, 520)#FÁ-cima
+winsound.Beep(293, 520)#D-cima
+winsound.Beep(349, 520)#F-cima
 time.sleep(0.1)
-winsound.Beep(329, 520)#MI-cima
-winsound.Beep(293, 520)#RÉ-cima
-winsound.Beep(349, 520)#FÁ-cima
-winsound.Beep(293, 520)#RÉ-cima
+winsound.Beep(329, 520)#E-cima
+winsound.Beep(293, 520)#D-cima
+winsound.Beep(349, 520)#F-cima
+winsound.Beep(293, 520)#D-cima
 time.sleep(0.1)
-winsound.Beep(329, 520)#MI-cima
-winsound.Beep(293, 520)#RÉ-cima
-winsound.Beep(233, 520)#SiB-cima
-winsound.Beep(261, 520)#DÓ-cima
+winsound.Beep(329, 520)#E-cima
+winsound.Beep(293, 520)#D-cima
+winsound.Beep(233, 520)#BB-cima
+winsound.Beep(261, 520)#C-cima
 time.sleep(0.1)
-winsound.Beep(220, 840)#LÁ-cima
+winsound.Beep(220, 840)#A-cima
 time.sleep(0.4)
 
-winsound.Beep(220, 780)#LÁ-cima
-winsound.Beep(293, 520)#RÉ-cima
-winsound.Beep(349, 520)#FÁ-cima
+winsound.Beep(220, 780)#A-cima
+winsound.Beep(293, 520)#D-cima
+winsound.Beep(349, 520)#F-cima
 time.sleep(0.1)
-winsound.Beep(329, 520)#MI-cima
-winsound.Beep(293, 520)#RÉ-cima
-winsound.Beep(349, 520)#FÁ-cima
-winsound.Beep(293, 520)#RÉ-cima
+winsound.Beep(329, 520)#E-cima
+winsound.Beep(293, 520)#D-cima
+winsound.Beep(349, 520)#F-cima
+winsound.Beep(293, 520)#D-cima
 time.sleep(0.1)
-winsound.Beep(329, 520)#MI-cima
-winsound.Beep(293, 520)#RÉ-cima
-winsound.Beep(220, 520)#LÁ-cima
-winsound.Beep(207, 580)#LáB-cima
+winsound.Beep(329, 520)#E-cima
+winsound.Beep(293, 520)#D-cima
+winsound.Beep(220, 520)#A-cima
+winsound.Beep(207, 580)#AB-cima
 time.sleep(0.1)
-winsound.Beep(196, 840)#SOL-cima
+winsound.Beep(196, 840)#G-cima
 time.sleep(0.4)
 
-winsound.Beep(196, 780)#SOL-cima
+winsound.Beep(196, 780)#G-cima
 time.sleep(0.1)
-winsound.Beep(233, 520)#SiB-cima
-winsound.Beep(261, 520)#Do#-cima
+winsound.Beep(233, 520)#BB-cima
+winsound.Beep(261, 520)#C#-cima
 time.sleep(0.1)
-winsound.Beep(329, 840)#MI-cima
+winsound.Beep(329, 840)#E-cima
 time.sleep(0.4)
 
-winsound.Beep(196, 780)#SOL-cima
-winsound.Beep(233, 520)#SiB-cima
-winsound.Beep(261, 520)#Do#-cima
+winsound.Beep(196, 780)#G-cima
+winsound.Beep(233, 520)#BB-cima
+winsound.Beep(261, 520)#C#-cima
 time.sleep(0.1)
-winsound.Beep(293, 840)#RÉ-cima
+winsound.Beep(293, 840)#D-cima
 time.sleep(0.4)
 
-winsound.Beep(146, 780)#RÉ-baixo
+winsound.Beep(146, 780)#D-baixo
 time.sleep(0.1)
-winsound.Beep(174, 520)#FÁ-baixo
-winsound.Beep(261, 520)#DÓ-cima
+winsound.Beep(174, 520)#F-baixo
+winsound.Beep(261, 520)#C-cima
 time.sleep(0.2)
-winsound.Beep(233, 520)#SiB-cima
-winsound.Beep(220, 520)#LÁ-cima
-winsound.Beep(261, 520)#DÓ-cima
-winsound.Beep(233, 520)#SiB-cima
+winsound.Beep(233, 520)#BB-cima
+winsound.Beep(220, 520)#A-cima
+winsound.Beep(261, 520)#C-cima
+winsound.Beep(233, 520)#BB-cima
 time.sleep(0.1)
-winsound.Beep(233, 520)#SiB-cima
-winsound.Beep(220, 520)#LÁ-cima
-winsound.Beep(220, 520)#LÁ-cima
-winsound.Beep(130, 520)#Dó#-baixo
+winsound.Beep(233, 520)#BB-cima
+winsound.Beep(220, 520)#A-cima
+winsound.Beep(220, 520)#A-cima
+winsound.Beep(130, 520)#C#-baixo
 time.sleep(0.1)
-winsound.Beep(293, 960)#RÉ-cima
-print("Linda música não é mesmo???")
+winsound.Beep(293, 960)#D-cima
