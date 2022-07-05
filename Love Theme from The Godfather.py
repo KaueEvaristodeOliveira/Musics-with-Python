49b86e171a994f8bb75325b4eaf675612e5e97bb
Kaue Evaristo <69019326+KaueEvaristodeOliveira@users.noreply.github.com>
2022-07-04 23:17:05 -0300

    Initial commit

---
+++ Love Theme from The Godfather.py
@@ -0,0 +1,195 @@
+import winsound
+import time
+
+#INTRO
+winsound.Beep(100, 240)
+time.sleep(0.5)
+winsound.Beep(1000, 60)
+time.sleep(0.5)
+winsound.Beep(1000, 60)
+time.sleep(0.5)
+winsound.Beep(100, 240)
+time.sleep(0.5)
+winsound.Beep(1000, 60)
+time.sleep(0.5)
+winsound.Beep(1000, 60)
+time.sleep(0.5)
+winsound.Beep(100, 240)
+time.sleep(0.5)
+winsound.Beep(1000, 60)
+time.sleep(0.5)
+winsound.Beep(1000, 60)
+time.sleep(0.5)
+
+#First Part
+winsound.Beep(220, 780)#LÁ-cima
+time.sleep(0.1)
+winsound.Beep(293, 520)#RÉ-cima
+winsound.Beep(349, 520)#FÁ-cima
+time.sleep(0.1)
+winsound.Beep(329, 520)#MI-cima
+winsound.Beep(293, 520)#RÉ-cima
+winsound.Beep(349, 520)#FÁ-cima
+winsound.Beep(293, 520)#RÉ-cima
+time.sleep(0.1)
+winsound.Beep(329, 520)#MI-cima
+winsound.Beep(293, 520)#RÉ-cima
+winsound.Beep(233, 520)#SiB-cima
+winsound.Beep(261, 520)#DÓ-cima
+time.sleep(0.1)
+winsound.Beep(220, 840)#LÁ-cima
+time.sleep(0.4)
+
+#Second Part
+winsound.Beep(220, 780)#LÁ-cima
+winsound.Beep(293, 520)#RÉ-cima
+winsound.Beep(349, 520)#FÁ-cima
+time.sleep(0.1)
+winsound.Beep(329, 520)#MI-cima
+winsound.Beep(293, 520)#RÉ-cima
+winsound.Beep(349, 520)#FÁ-cima
+winsound.Beep(293, 520)#RÉ-cima
+time.sleep(0.1)
+winsound.Beep(329, 520)#MI-cima
+winsound.Beep(293, 520)#RÉ-cima
+winsound.Beep(220, 520)#LÁ-cima
+winsound.Beep(207, 580)#LáB-cima
+time.sleep(0.1)
+winsound.Beep(196, 840)#SOL-cima
+time.sleep(0.4)
+
+#Third Part
+winsound.Beep(196, 780)#SOL-cima
+winsound.Beep(233, 520)#SiB-cima
+winsound.Beep(261, 520)#Do#-cima
+time.sleep(0.1)
+winsound.Beep(329, 840)#MI-cima
+time.sleep(0.4)
+
+#Fourth Part
+winsound.Beep(196, 780)#SOL-cima
+winsound.Beep(233, 520)#SiB-cima
+winsound.Beep(261, 520)#Do#-cima
+time.sleep(0.1)
+winsound.Beep(293, 840)#RÉ-cima
+time.sleep(0.3)
+
+#Fifth Part
+winsound.Beep(146, 780)#RÉ-baixo
+time.sleep(0.1)
+winsound.Beep(174, 520)#FÁ-baixo
+winsound.Beep(261, 520)#DÓ-cima
+#CODA----------------------------
+time.sleep(0.1)
+winsound.Beep(233, 520)#SiB-cima
+winsound.Beep(220, 520)#LÁ-cima
+winsound.Beep(261, 520)#DÓ-cima
+winsound.Beep(233, 520)#SiB-cima
+time.sleep(0.1)
+winsound.Beep(233, 520)#SiB-cima
+winsound.Beep(220, 520)#LÁ-cima
+winsound.Beep(220, 520)#LÁ-cima
+winsound.Beep(130, 520)#Dó#-baixo
+time.sleep(0.1)
+winsound.Beep(146, 840)#RÉ-baixo
+time.sleep(0.3)
+
+#Sixth Part
+winsound.Beep(293, 840)#RÉ-cima
+winsound.Beep(293, 720)#RÉ-cima
+winsound.Beep(277, 720)#RéB-cima
+time.sleep(0.1)
+winsound.Beep(261, 720)#DÓ-cima
+time.sleep(0.1)
+winsound.Beep(329, 720)#MI-cima
+time.sleep(0.1)
+winsound.Beep(293, 720)#RÉ-cima
+winsound.Beep(233, 720)#SiB-cima
+time.sleep(0.1)
+winsound.Beep(220, 840)#LÁ-cima
+time.sleep(0.3)
+
+#Seventh Part
+winsound.Beep(220, 780)#LÁ-cima
+winsound.Beep(261, 520)#DÓ-cima
+winsound.Beep(220, 520)#LÁ-cima
+time.sleep(0.1)
+winsound.Beep(196, 840)#SOL-cima
+time.sleep(0.3)
+
+#Octave Part
+winsound.Beep(196, 780)#SOL-cima
+winsound.Beep(233, 520)#SiB-cima
+winsound.Beep(207, 520)#Sol#-cima
+time.sleep(0.1)
+winsound.Beep(220, 840)#LÁ-cima
+time.sleep(0.3)
+
+#Ninth Part
+winsound.Beep(220, 780)#LÁ-cima
+time.sleep(0.1)
+winsound.Beep(293, 520)#RÉ-cima
+winsound.Beep(349, 520)#FÁ-cima
+time.sleep(0.1)
+winsound.Beep(329, 520)#MI-cima
+winsound.Beep(293, 520)#RÉ-cima
+winsound.Beep(349, 520)#FÁ-cima
+winsound.Beep(293, 520)#RÉ-cima
+time.sleep(0.1)
+winsound.Beep(329, 520)#MI-cima
+winsound.Beep(293, 520)#RÉ-cima
+winsound.Beep(233, 520)#SiB-cima
+winsound.Beep(261, 520)#DÓ-cima
+time.sleep(0.1)
+winsound.Beep(220, 840)#LÁ-cima
+time.sleep(0.4)
+
+winsound.Beep(220, 780)#LÁ-cima
+winsound.Beep(293, 520)#RÉ-cima
+winsound.Beep(349, 520)#FÁ-cima
+time.sleep(0.1)
+winsound.Beep(329, 520)#MI-cima
+winsound.Beep(293, 520)#RÉ-cima
+winsound.Beep(349, 520)#FÁ-cima
+winsound.Beep(293, 520)#RÉ-cima
+time.sleep(0.1)
+winsound.Beep(329, 520)#MI-cima
+winsound.Beep(293, 520)#RÉ-cima
+winsound.Beep(220, 520)#LÁ-cima
+winsound.Beep(207, 580)#LáB-cima
+time.sleep(0.1)
+winsound.Beep(196, 840)#SOL-cima
+time.sleep(0.4)
+
+winsound.Beep(196, 780)#SOL-cima
+time.sleep(0.1)
+winsound.Beep(233, 520)#SiB-cima
+winsound.Beep(261, 520)#Do#-cima
+time.sleep(0.1)
+winsound.Beep(329, 840)#MI-cima
+time.sleep(0.4)
+
+winsound.Beep(196, 780)#SOL-cima
+winsound.Beep(233, 520)#SiB-cima
+winsound.Beep(261, 520)#Do#-cima
+time.sleep(0.1)
+winsound.Beep(293, 840)#RÉ-cima
+time.sleep(0.4)
+
+winsound.Beep(146, 780)#RÉ-baixo
+time.sleep(0.1)
+winsound.Beep(174, 520)#FÁ-baixo
+winsound.Beep(261, 520)#DÓ-cima
+time.sleep(0.2)
+winsound.Beep(233, 520)#SiB-cima
+winsound.Beep(220, 520)#LÁ-cima
+winsound.Beep(261, 520)#DÓ-cima
+winsound.Beep(233, 520)#SiB-cima
+time.sleep(0.1)
+winsound.Beep(233, 520)#SiB-cima
+winsound.Beep(220, 520)#LÁ-cima
+winsound.Beep(220, 520)#LÁ-cima
+winsound.Beep(130, 520)#Dó#-baixo
+time.sleep(0.1)
+winsound.Beep(293, 960)#RÉ-cima
+print("Linda música não é mesmo???")
